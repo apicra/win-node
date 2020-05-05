93f2963acc166b6e3667c979b4939bdd080c2301
Softreck OÜ <52911890+softreck@users.noreply.github.com>
2020-05-05 12:59:34 +0200

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,11 @@
+setlocal
+
+set NODEJS_VERSION=v12.16.3
+set NODEJS_FILENAME=node-%NODEJS_VERSION%-x64.msi
+set NODEJS_URL=https://nodejs.org/dist/%NODEJS_VERSION%/%NODEJS_FILENAME%
+set NODEJS_DOWNLOAD_LOCATION=C:\
+
+powershell -NoExit -Command "(New-Object Net.WebClient).DownloadFile('%NODEJS_URL%', '%NODEJS_DOWNLOAD_LOCATION%%NODEJS_FILENAME%'); exit;"
+msiexec /qn /l* C:\node-log.txt /i %NODEJS_DOWNLOAD_LOCATION%%NODEJS_FILENAME%
+
+endlocal
