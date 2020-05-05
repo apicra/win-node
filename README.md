4ff0cc232b33e621b7f8de9d5beb8f479b2bd152
tom <tom@sapletta.com>
2020-05-05 13:12:48 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # win-node
 Node JS for windows OS
+
+curl git
+
+curl git
--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
+@echo off
 setlocal
 
 set NODEJS_VERSION=v12.16.3
@@ -5,7 +6,15 @@ set NODEJS_FILENAME=node-%NODEJS_VERSION%-x64.msi
 set NODEJS_URL=https://nodejs.org/dist/%NODEJS_VERSION%/%NODEJS_FILENAME%
 set NODEJS_DOWNLOAD_LOCATION=C:\
 
-powershell -NoExit -Command "(New-Object Net.WebClient).DownloadFile('%NODEJS_URL%', '%NODEJS_DOWNLOAD_LOCATION%%NODEJS_FILENAME%'); exit;"
-msiexec /qn /l* C:\node-log.txt /i %NODEJS_DOWNLOAD_LOCATION%%NODEJS_FILENAME%
+curl %NODEJS_URL%
+msiexec /qn /l* C:\node-log.txt /i %NODEJS_FILENAME%
+
+::, '%NODEJS_DOWNLOAD_LOCATION%%NODEJS_FILENAME%'
+::%NODEJS_FILENAME%
+
+::powershell -NoExit -Command "(New-Object Net.WebClient).DownloadFile('%NODEJS_URL%', '%NODEJS_DOWNLOAD_LOCATION%%NODEJS_FILENAME%'); exit;"
+::msiexec /qn /l* C:\node-log.txt /i %NODEJS_DOWNLOAD_LOCATION%%NODEJS_FILENAME%
 
 endlocal
+
+dir
