bfb58823d893f84a1b18f94bc15f47ee8808a26b
tom <tom@sapletta.com>
2020-05-07 17:14:36 +0200

    update

--- download.bat
+++ download.bat
@@ -2,13 +2,6 @@
 :: silent install nodejs on windows console
 :: https://www.ibm.com/support/knowledgecenter/SSZUMP_7.2.1/install_grid_sym/install_silent.html
 
-NET SESSION >nul 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-	echo This setup needs admin permissions. Please run this file as admin.
-	pause
-	exit
-)
-
 setlocal
 
 set NODEJS_VERSION=v12.16.3
@@ -21,3 +14,7 @@ set INSTALLDIR=C:\nodejs\
 @echo on
 
 curl.bat %NODEJS_URL% %NODEJS_FILENAME%
+echo file %NODEJS_FILENAME% is downloaded!
+
+endlocal
+dir
